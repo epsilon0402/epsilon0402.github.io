c30a575619d5d624b06dcbd10ab2458584f8f473
epsilon0402 <eulug.vn@gmail.com>
2025-11-21 11:26:00 +0700

    setup new blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 import type { IconMap, SocialLink, Site } from '@/types'
 
 export const SITE: Site = {
-  title: 'astro-erudite',
+  title: 'epsilon0402',
   description:
-    'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
+    'Epsilon, tức là rất nhỏ, nhưng không bằng 0. Và nhiều epsilon cộng lại có thể trở thành những cái đáng kể. Có thể là 1, là 2, có thể là vô cùng. Điều quan trọng là ta có biết cách kết hợp các epsilon khác nhau lại hay không',
   href: 'https://astro-erudite.vercel.app',
-  author: 'jktrn',
+  author: 'epsilon0402',
   locale: 'en-US',
   featuredPostCount: 2,
   postsPerPage: 3,
@@ -28,15 +28,15 @@ export const NAV_LINKS: SocialLink[] = [
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    href: 'https://github.com/jktrn',
+    href: 'https://github.com/epsilon0402',
     label: 'GitHub',
   },
   {
-    href: 'https://twitter.com/enscry',
+    href: 'https://twitter.com/epsilon0402',
     label: 'Twitter',
   },
   {
-    href: 'mailto:jason@enscribe.dev',
+    href: 'mailto:epsilon0402@gmail.com',
     label: 'Email',
   },
   {
--- src/content/authors/enscribe.md
+++ src/content/authors/enscribe.md
@@ -1,10 +1,10 @@
 ---
-name: 'enscribe'
-pronouns: 'he/him'
+name: 'epsilon0402'
+pronouns: 'she/her'
 avatar: 'https://gravatar.com/avatar/9bfdc4ec972793cf05cb91efce5f4aaaec2a0da1bf4ec34dad0913f1d845faf6.webp?size=256'
 bio: 'd(-_-)b'
-website: 'https://enscribe.dev'
-twitter: 'https://twitter.com/enscry'
-github: 'https://github.com/jktrn'
-mail: 'jason@enscribe.dev'
+website: 'https://epsilon0402.github.io'
+twitter: 'https://twitter.com/epsilon0402'
+github: 'https://github.com/epsilon0402'
+mail: 'epsilon0402@gmail.com'
 ---
